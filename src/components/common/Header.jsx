8a0610af72857cb66fb174ed2504233dba52577c
Rutuja51 <rutujarmule51@gmail.com>
2026-08-24 22:20:40 +0200

    added responsiveness in cart

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router";
+import { useState } from "react";
 export default function Header() {
     // counterstore or cartStore defined in store.js cart or counter are states defined in slice
     let myData= useSelector((myStore)=>(myStore.counterStore.count))
     let myCartData= useSelector((myStore)=>(myStore.cartStore.cart))
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <div>
             <nav className="bg-neutral-primary w-full z-20 top-0 start-0 border-default">
@@ -19,12 +21,14 @@ export default function Header() {
                             Flowbite
                         </span>
                     </a>
+                    {/* hamburger menu */}
                     <button
                         data-collapse-toggle="navbar-dropdown"
                         type="button"
                         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
                         aria-controls="navbar-dropdown"
-                        aria-expanded="false"
+                        aria-expanded={isMenuOpen}
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
                     >
                         <span className="sr-only">Open main menu</span>
                         <svg
@@ -44,7 +48,8 @@ export default function Header() {
                             />
                         </svg>
                     </button>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                    {/* menu */}
+                    <div className={`${isMenuOpen ? "block" : "hidden"} w-full md:block md:w-auto`} id="navbar-dropdown">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
                             <li>
                                 <Link to={'/'}
